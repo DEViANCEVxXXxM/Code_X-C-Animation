412d563e40766e0cca113f802ee5192cd060e71a
DEViANCE <DEViANCEVxXXxM@Gmail.com>
2017-02-03 15:12:19 +0500

    Добавьте файлы проекта.

---
+++ Проект1/main.cpp
@@ -0,0 +1,35 @@
+// Сделал Репринцев Никита.
+#include <iostream>
+#include <stdlib.h>
+#include "windows.h"
+
+using namespace std;
+
+void main()
+{
+	for (int a = 0; a < 9999; a++)
+	{
+		cout << "                                                                          \n";
+		cout << "     @@@@@@@@@@@  @@@@@@@@@@         @@  @@@@@@@@@           @@@@   @@@@  \n";
+		Sleep(75);
+		cout << "     @@       @@  @@      @@         @@  @@                     @@ @@     \n";
+		Sleep(75);
+		cout << "     @@           @@      @@         @@  @@                      @@@      \n";
+		Sleep(75);
+		cout << "     @@           @@      @@  @@@@@@@@@  @@@@@@@@                @@@     \n";
+		Sleep(75);
+		cout << "     @@           @@      @@  @@     @@  @@                     @@ @@     \n";
+		Sleep(75);
+		cout << "     @@       @@  @@      @@  @@     @@  @@                    @@   @@    \n";
+		Sleep(75);
+		cout << "     @@@@@@@@@@@  @@@@@@@@@@  @@@@@@@@@  @@@@@@@@@  @@@@@@@  @@@     @@@   \n";
+		Sleep(75);
+		cout << "                                                                          \n";
+		Sleep(75);
+		cout << "                                                                          \n";
+
+		system("cls");
+	}
+	system("pause");
+}
+
